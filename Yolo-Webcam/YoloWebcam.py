5a2ad39e34298140f2a1b4adf340435942eccc44
HEMANT <webdev.hemant@gmail.com>
2023-10-30 16:52:30 +0530

    mobile cam

--- Yolo-Webcam/YoloWebcam.py
+++ Yolo-Webcam/YoloWebcam.py
@@ -2,13 +2,33 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("http://192.168.0.103:4747/video")
 cap.set(3,1280)
 cap.set(4,720)
 
 model = YOLO("../Yolo-Weights/yolov8l.pt")
 
+
 while True:
     success, img = cap.read()
+    
+    if not success:
+        print("Error: Could not read frame from the video source.")
+        break  # Break the loop if there's an issue with the video source.
+
+    # Perform object detection and display the frame.
+    results = model(img, stream=True)
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break  # Exit the loop when 'q' is pressed
+
+# Release the video capture and close OpenCV windows when done.
+cap.release()
+cv2.destroyAllWindows()
+
+# while True:
+#     success, img = cap.read()
+#     results = model(img, stream=True)
+#     cv2.imshow("Image", img)
+#     cv2.waitKey(1)
